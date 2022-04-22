1b42015165eca41bf269a56143c5ea781d494281
Douglas Rios <58635327+DrIOSX@users.noreply.github.com>
2022-04-21 21:23:23 -0500

    Update: Write-Output

--- src/modules/ActiveUsersAudit.psm1
+++ src/modules/ActiveUsersAudit.psm1
@@ -70,7 +70,7 @@ function Get-ActiveUsersAudit {
             HelpMessage = "Active Directory User Enabled or not",
             ValueFromPipelineByPropertyName = $true
         )]
-        [bool]$Enabled = $true,
+        [bool]$Enabled = $false,
         [Parameter( Position = '2', ParameterSetName = 'URL Key Vault')]
         [Parameter( Position = '2', ParameterSetName = 'Password')]
         [Parameter(
@@ -199,6 +199,7 @@ function Get-ActiveUsersAudit {
         $time = (Get-Date).Adddays( - ($DaysInactive))
         # Add Datetime to filename
         $csv = "$($csvPath).$((Get-Date).ToString('yyyy-MM-dd.hh.mm.ss'))"
+        Write-Output "Searching for users where Enabled = $Enabled"
         # Audit Script with export to csv and zip. Paramters for Manager, lastLogonTimestamp and DistinguishedName normalized.
         Get-aduser -Filter { LastLogonTimeStamp -lt $time -and Enabled -eq $Enabled } -Properties `
             GivenName, Surname, Mail, UserPrincipalName, Title, OfficePhone, MobilePhone, Description, Manager, lastlogontimestamp, samaccountname, DistinguishedName   | `
